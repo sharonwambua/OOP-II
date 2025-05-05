c4dbc520cfe23fdb77c38ed8b017c5a764c6bb61
sharonwambua <159538532+sharonwambua@users.noreply.github.com>
2025-05-05 14:28:42 +0300

    Update challenge3.java

--- challenge3.java
+++ challenge3.java
@@ -1,1 +1,25 @@
+import javax.swing.JOptionPane;
+import java.util.Scanner;
 
+public class MyGUI {
+    public static void main(String[] args) {
+     JOptionPane.showMessageDialog(null,"Welcome to Java programming");
+        JOptionPane.showInputDialog(null,"what is your name?");
+        int Age;
+
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Age?");
+        Age = input.nextInt();
+
+        if (Age<18){
+            System.out.println("Your age is " + Age);
+            System.out.println("Not adult");
+        }
+else{
+    System.out.println("Your age is " + Age);
+    System.out.println("Adult");
+}
+       
+    }
+}
