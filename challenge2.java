1d32b476e160f5242905567794c43e3ba8ceb392
sharonwambua <159538532+sharonwambua@users.noreply.github.com>
2025-05-05 14:20:25 +0300

    Create challenge2.java

---
+++ challenge2.java
@@ -0,0 +1,16 @@
+class Variables{
+void showInterest(){
+  double amount;
+  double interest;
+  amount = 1200.00;
+  system.out.println("amount:" +amount);
+  interst =3.0;
+  amount = amount*(100+interest)/100;
+  system.out.println("after 2 years:" + amount);
+  amount = amount*(100+interest)/100;
+  system.out.println("after 2 years:"+ amount);
+  public static void main (string[] args){
+    (new Variables()).showInterest();
+  }
+}
+}
